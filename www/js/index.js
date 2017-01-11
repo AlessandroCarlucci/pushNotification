e997736d8d8b0915bde4268abfda730f8bc26a32
AlessandroCarlucci <ing.alessandro.carlucci@gmail.com>
2017-01-11 10:58:44 +0100

    dfsd

--- www/js/index.js
+++ www/js/index.js
@@ -34,19 +34,7 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
-        window.FirebasePlugin.grantPermission();
-        window.FirebasePlugin.setBadgeNumber(3);
-        window.FirebasePlugin.getToken(function(token) {
-    // save this server-side and use it to push notifications to this device
-    alert(token);
-}, function(error) {
-    alert(error);
-});
-window.FirebasePlugin.onNotificationOpen(function(notification) {
-    alert(notification);
-}, function(error) {
-    alert(error);
-});
+        app.setupPush();
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
@@ -58,7 +46,55 @@ window.FirebasePlugin.onNotificationOpen(function(notification) {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
-    }
+    },
 
+setupPush: function() {
+        console.log('calling push init');
+        var push = PushNotification.init({
+            "android": {
+                "senderID": "161407445614"
+            },
+            "browser": {},
+            "ios": {
+                "sound": true,
+                "vibration": true,
+                "badge": true
+            },
+            "windows": {}
+        });
+        console.log('after init');
+
+        push.on('registration', function(data) {
+            console.log('registration event: ' + data.registrationId);
+
+            var oldRegId = localStorage.getItem('registrationId');
+            if (oldRegId !== data.registrationId) {
+                // Save new registration ID
+                localStorage.setItem('registrationId', data.registrationId);
+                // Post registrationId to your app server as the value has changed
+            }
+
+            var parentElement = document.getElementById('registration');
+            var listeningElement = parentElement.querySelector('.waiting');
+            var receivedElement = parentElement.querySelector('.received');
+
+            listeningElement.setAttribute('style', 'display:none;');
+            receivedElement.setAttribute('style', 'display:block;');
+        });
+
+        push.on('error', function(e) {
+            console.log("push error = " + e.message);
+        });
+
+        push.on('notification', function(data) {
+            console.log('notification event');
+            navigator.notification.alert(
+                data.message,         // message
+                null,                 // callback
+                data.title,           // title
+                'Ok'                  // buttonName
+            );
+       });
+    }
     
 };
